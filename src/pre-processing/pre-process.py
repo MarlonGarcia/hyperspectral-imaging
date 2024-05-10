6b0949e379e7e1e81071d7ce217fe247c9114f3f
Marlon Rodrigues Garcia <marlonrg@gmail.com>
2024-05-10 16:59:59 -0300

    changes in comments

--- src/pre-processing/pre-process.py
+++ src/pre-processing/pre-process.py
@@ -15,29 +15,91 @@ import matplotlib.pyplot as plt
 class hipercube:
     
     def __init__(self, image_dir):
+        '''
+        Initializing function
+
+        Parameters
+        ----------
+        image_dir : 'string'
+            Directory to the images.
+        '''
         self.dir = image_dir
     
     def load(self):
+        '''
+        Loading hyperspectral image
+
+        Returns
+        -------
+        I : 'numpy.ndarray'
+            Hyperspectral image in (C, H, W) format, with C channels (images),
+            with a hight H and a width W.
+        '''
+        # First, listing all the names of files in the image directory
         image_names = os.listdir(self.dir)
+        # Changing to the images directory
         os.chdir(self.dir)
+        # Creating the variable to contain the images
         I = []
+        # Adding all images with name 'name' from 'self.dir' directory to 'I'
         for name in image_names:
             I.append(cv2.imread(name, cv2.IMREAD_GRAYSCALE))
+        # Returning the hiperspectral cube 'I'
         return np.asarray(I)
     
     def get_rgb(self, I, w = [5, 15, 23]):
-        # Considering RGB not BGR (opencv)
+        '''
+        Getting an RGB image from the hypercube
+
+        Parameters
+        ----------
+        I : 'numpy.ndarray'
+            Hyperspectral cube in (C, H, W) format, with C channels (images),
+            with a hight H and a width W.
+        w : 'list', optional
+            A list with the position of the wavelengths to be considered as
+            red, green and blue, respectivelly. The default is [5, 15, 23].
+
+        Returns
+        -------
+        'numpy.ndarray'
+            RGB image from the hypercube.
+        '''
+        # Getting a (C, H, W) format (full RGB) from a (H, W) image format
+        # (each color or wavelength) using 'expand_dims'
         Ib = np.expand_dims(I[w[0]], axis = 2)    #450
         Ig = np.expand_dims(I[w[1]], axis = 2)    #550
         Ir = np.expand_dims(I[w[2]], axis = 2)    #630
+        # Returning the full RGB image
         return np.concatenate((Ir, Ig, Ib), axis = 2)
     
-    def hist(self, I, pd = 256):
-        # This code consider a pixel detph of 8 bits
-        # Preparing the variable
-        hist = np.zeros([len(I), pd])
+    def hist(self, I, pd = 8):
+        '''
+        Getting the histogram for each channel
+
+        Parameters
+        ----------
+        I : 'numpy.ndarray'
+            Hyperspectral cube in (C, H, W) format, with C channels (images),
+            with a hight H and a width W.
+        pd : 'int', optional
+            Camera's pixel depth. The default is 8 bits.
+
+        Returns
+        -------
+        hist : 'numpy.ndarray'
+            A (C, V) variable with the histogram for pixels' intensities in the
+            V axis, for each channel C.
+        '''
+        # Defining a null variable
+        hist = np.zeros([len(I), 2**pd])
+        # Adding histograms to 'hist', channel by channel
         for n in range(len(I)):
-            temp = np.histogram(I[n], bins = np.linspace(0, 256, 257))
+            # Using 'np.linspace' to create the intensity vector of a histogram
+            # and 'np.histogram' to calculate it for a given channel 'n'
+            # OBS: The total number of points is 2**pd+1
+            temp = np.histogram(I[n], bins = np.linspace(0, 2**pd, 2**pd+1))
+            # from 'temp'  variable to 'hist' variable, in channel 'n'
             hist[n, :] = temp[0]
         return hist
     
